2a60fa3047fb6703d888567310df546fd6a7f778
Lee Prout <wleeprout@gmail.com>
2021-03-21 18:58:54 -0400

    created postgres db

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+# app.config('SQLALCHEMY_DATABASE_URI') = environ.get('DATABASE_URL', 'sqlite:///leaflet.sqlite')
+
+db = SQLAlchemy(app)
+class Quakes(db.Model):
+    __tablename__ = 'quakes'
+
+
 
 @app.route('/')
 def index(): 
     return render_template('index.html')
+
+# @app.route('/api/quakes')    
 
 if __name__ == '__main__':   
     app.run(debug=True)
